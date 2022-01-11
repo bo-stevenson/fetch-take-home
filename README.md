06ce00c0f01257f20fbd8ade7323da08ef267bc9
Bo Stevenson <bostevenson1027@gmail.com>
2022-01-11 14:37:46 -0600

    added post route and README

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# fetch-take-home
+# User-Creation-Form
+
+## Overview
+This repository is for a webpage to display a form that does not allow form submission without having been completed. The form data will the be posted to an endpoint.
+
+## Table of Contents
+
+1. [Installation](#installation)
+
+2. [Usage](#usage)
+
+3. [Contributions](#contribution)
+
+4. [Test Instructions](#test-instructions)
+
+5. [Screenshots](#screenshots)
+
+6. [Licenses](#licenses)
+
+7. [Questions](#questions)
+
+
+## Installation
+
+ git clone the repo 
+
+## Usage
+
+Click on the form boxes and fill out the information. Then, click submit and the bottom to send your data to the endpoint.
+
+## Contribution
+
+none
+
+## Test Instructions
+
+none
+
+## Screenshots
+
+![Screenshot](assets/images/screenshot.png)
+## License
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+    
+## Questions? Contact Me!
+
+My GitHub is [bo-stevenson](https://www.github.com/bo-stevenson).
+
+My email is [bostevenson1027@gmail.com](mailto:bostevenson1027@gmail.com).
--- index.js
+++ index.js
@@ -1,12 +1,76 @@
 
+const form = document.getElementById('form');
+const fullName = document.getElementById('fullName');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const occupation = document.getElementById('selectOccupation');
+const state = document.getElementById('selectState');
+//listens for submit button 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log("button pressed!")
+    checkInputs();
+    
+});
+
+function checkInputs() {
+    //get values from the inputs
+    const fullNameValue = fullName.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+    //check if values are empty
+    if(fullNameValue === "") {
+        
+        setError(fullName, "Name cannot be blank");
+    } else {
+        setSuccess(fullName);
+        
+    }
+
+    if(emailValue === "") {
+        
+        setError(email, "Email cannot be blank");
+    } else {
+        setSuccess(email);
+        
+    }
+
+    if(passwordValue === "") {
+        
+        setError(password, "Password cannot be blank");
+    } else {
+        setSuccess(password);
+        postForm();
+    }
+
+
+}
+
+//sets css to display error
+function setError(input, message) {
+    console.log(message);
+    const formControl = input.parentElement; //.formcontrol
+    const small = formControl.querySelector('small');
 
 
 
+    small.innerText = message;
+    
+    formControl.className = 'form-control error';
+}
+//sets css to display success
+function setSuccess(input){
+    const formControl = input.parentElement;
+    
+    formControl.className = "form-control success";
 
+}
+//Function to get occupation and state lists and append to select
 
 getFormInfo = () => {
 
-let queryURL = "https://cors-anywhere.herokuapp.com/" + "https://frontend-take-home.fetchrewards.com/form";
+let queryURL = "https://cors-anywhere.herokuapp.com/"+"https://frontend-take-home.fetchrewards.com/form";
 $.ajax({
     url:queryURL,
     method: "GET"
@@ -14,12 +78,12 @@ $.ajax({
     console.log("GET request failed");
 }).done(function (response) {
     console.log("GET request successful");
-
+    
     let states = response.states;
     let occupations = response.occupations;
     let selectOct = document.getElementById("selectOccupation");
     let selectState = document.getElementById("selectState");
-
+    //loop through occupations and append to select tag
     for(let i=0; i<occupations.length; i++) {
         let option = occupations[i];
         let el = document.createElement("option");
@@ -27,7 +91,7 @@ $.ajax({
         el.value = option;
         selectOct.appendChild(el);
     }
-
+    //loop through states and append to select tag
     for(let i=0; i<states.length; i++) {
         let option = states[i];
         let el = document.createElement("option")
@@ -39,4 +103,29 @@ $.ajax({
 
 })
 }
+//function postForm 
+const postForm = () => {
+    let newUser = {
+       "name": fullName.value.trim(),
+       "email": email.value.trim(), 
+       "password": password.value.trim(),
+       "occupation": occupation.value.trim(),
+       "state": state.value.trim()
+   };
+   
+   console.log(newUser);
+   return $.ajax({
+       url: "https://cors-anywhere.herokuapp.com/"+"https://frontend-take-home.fetchrewards.com/form",
+       data: JSON.stringify(newUser),
+       method: "POST",
+       contentType: "application/json",
+       //dataType:"JSON",
+    
+   }).done(
+       console.log("Post success!")
+   );
+};
+
 getFormInfo();
+
+
